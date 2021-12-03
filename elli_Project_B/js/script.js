edbf42ee4af6ad7918bdffb40fe08df950d40b2d
elli-halperin <ceh530@nyu.edu>
2021-12-03 03:43:10 -0800

    prototype

---
+++ elli_Project_B/js/script.js
@@ -0,0 +1,30 @@
+/*function setup() {
+  createCanvas(400, 500);
+  background(220);
+}
+
+function draw() {
+  background(220);
+}
+
+*/
+
+function openDoor1(){
+  window.open("1.html", "_self");
+}
+
+function openDoor2(){
+  window.open("2.html", "_self");
+}
+
+function openDoor3(){
+  window.open("3.html", "_self");
+}
+
+function openBadDoor(){
+  window.open("bad.html", "_self");
+}
+
+function openGoodDoor(){
+  window.open("good.html", "_self");
+}
